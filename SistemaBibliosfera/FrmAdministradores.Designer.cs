ebccd5c9ed157ad858f052d5b64559bed20cc159
Sara Avila <lasaraavila1@gmail.com>
2025-10-29 11:49:23 -0600

    Componentes al formulario FrmAdministradores

--- SistemaBibliosfera/FrmAdministradores.Designer.cs
+++ SistemaBibliosfera/FrmAdministradores.Designer.cs
@@ -28,23 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.DtgDatos = new System.Windows.Forms.DataGridView();
+            this.TxtUsuaio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Busqueda de bibliotecarios";
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(549, 165);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(88, 31);
+            this.BtnBuscar.TabIndex = 1;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Location = new System.Drawing.Point(662, 165);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(90, 31);
+            this.BtnAgregar.TabIndex = 2;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // DtgDatos
+            // 
+            this.DtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtgDatos.Location = new System.Drawing.Point(214, 214);
+            this.DtgDatos.Name = "DtgDatos";
+            this.DtgDatos.Size = new System.Drawing.Size(893, 150);
+            this.DtgDatos.TabIndex = 3;
+            // 
+            // TxtUsuaio
+            // 
+            this.TxtUsuaio.Location = new System.Drawing.Point(214, 170);
+            this.TxtUsuaio.Name = "TxtUsuaio";
+            this.TxtUsuaio.Size = new System.Drawing.Size(310, 26);
+            this.TxtUsuaio.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre de Usuario";
             // 
             // FrmAdministradores
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtUsuaio);
+            this.Controls.Add(this.DtgDatos);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmAdministradores";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmAdministradores";
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.DataGridView DtgDatos;
+        private System.Windows.Forms.TextBox TxtUsuaio;
+        private System.Windows.Forms.Label label2;
     }
 }
