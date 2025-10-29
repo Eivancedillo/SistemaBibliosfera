99ef8f5fff810df4401a98e6551f779dd92cd02e
Diana Veloz <dvelozpadilla@gmail.com>
2025-10-29 10:17:08 -0600

    Creacion de entidad Autor - Entidad creada en su totalidad.

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -8,5 +8,14 @@ namespace Entidades
 {
     public class Autor
     {
+        public Autor(int idAutor, string nombreAutor)
+        {
+            IdAutor = idAutor;
+            NombreAutor = nombreAutor;
+        }
+
+        public int IdAutor { get; set; }
+        public string NombreAutor { get; set; }
+
     }
 }
