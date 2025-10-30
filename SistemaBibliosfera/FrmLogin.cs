5affe04a64bc3896212185db1bb90351bbd8a3f3
Eivan <eivan.ecedillo@gmail.com>
2025-10-30 12:50:44 -0600

    Revert "Confirmacion"
    This reverts commit 1c36026e59b8f272218bfc2afcefe9c0daa4fc29.

--- SistemaBibliosfera/FrmLogin.cs
+++ SistemaBibliosfera/FrmLogin.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Manejadores;
@@ -14,9 +15,49 @@ namespace SistemaBibliosfera
     public partial class FrmLogin : Form
     {
         ManejadorLogin Ml;
+        int contador = 0;
         public FrmLogin()
         {
             InitializeComponent();
+            Ml = new ManejadorLogin();
+        }
+
+        private void BtnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TxtUsuario.Text) || string.IsNullOrWhiteSpace(TxtContrasena.Text))
+            {
+                MessageBox.Show("¡Rellene todos los campos!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (Ml.Validar(TxtUsuario, TxtContrasena))
+            {
+                FrmPrincipal ia = new FrmPrincipal();
+                ia.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Error de credenciales..");
+                contador++;
+                if (contador >= 2)
+                {
+                    MessageBox.Show("Sus credenciales se han bloqueado, espere 3 segundos");
+                    TxtUsuario.Enabled = false;
+                    TxtContrasena.Enabled = false;
+                    Thread.Sleep(3000);//dormir el hilo principal
+                    //despues de tipo habilitarlos
+                    MessageBox.Show("Ahora puede continuar..");
+                    TxtUsuario.Enabled = true;
+                    TxtContrasena.Enabled = true;
+                    contador = 0;
+
+                }
+            }
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
