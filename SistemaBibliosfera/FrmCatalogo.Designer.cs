4bc314c89abfe9d8141296a9dab0701bef18bff6
Eivan <eivan.ecedillo@gmail.com>
2025-11-03 19:55:59 -0600

    FrmCatalogo diseño inicial creado SistemaBibliosfera: - FrmCatalogo: diseño basico hecho

--- SistemaBibliosfera/FrmCatalogo.Designer.cs
+++ SistemaBibliosfera/FrmCatalogo.Designer.cs
@@ -28,7 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbFiltro = new System.Windows.Forms.ComboBox();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.DtgDatos = new System.Windows.Forms.DataGridView();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.TxtBuscar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CmbOrdenar = new System.Windows.Forms.ComboBox();
+            this.BtnAscDes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Catalogo";
+            // 
+            // CmbFiltro
+            // 
+            this.CmbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbFiltro.FormattingEnabled = true;
+            this.CmbFiltro.Location = new System.Drawing.Point(253, 90);
+            this.CmbFiltro.Margin = new System.Windows.Forms.Padding(4);
+            this.CmbFiltro.Name = "CmbFiltro";
+            this.CmbFiltro.Size = new System.Drawing.Size(160, 33);
+            this.CmbFiltro.TabIndex = 33;
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Location = new System.Drawing.Point(679, 334);
+            this.BtnCerrar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(100, 28);
+            this.BtnCerrar.TabIndex = 32;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = true;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // DtgDatos
+            // 
+            this.DtgDatos.AllowUserToAddRows = false;
+            this.DtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtgDatos.Location = new System.Drawing.Point(13, 131);
+            this.DtgDatos.Margin = new System.Windows.Forms.Padding(4);
+            this.DtgDatos.Name = "DtgDatos";
+            this.DtgDatos.RowHeadersWidth = 51;
+            this.DtgDatos.Size = new System.Drawing.Size(766, 185);
+            this.DtgDatos.TabIndex = 31;
+            this.DtgDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellClick);
+            this.DtgDatos.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellEnter);
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(679, 52);
+            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(100, 28);
+            this.BtnBuscar.TabIndex = 29;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // TxtBuscar
+            // 
+            this.TxtBuscar.Location = new System.Drawing.Point(13, 52);
+            this.TxtBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtBuscar.Name = "TxtBuscar";
+            this.TxtBuscar.Size = new System.Drawing.Size(658, 30);
+            this.TxtBuscar.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Filtro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Ordenar";
+            // 
+            // CmbOrdenar
+            // 
+            this.CmbOrdenar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbOrdenar.FormattingEnabled = true;
+            this.CmbOrdenar.Location = new System.Drawing.Point(511, 90);
+            this.CmbOrdenar.Margin = new System.Windows.Forms.Padding(4);
+            this.CmbOrdenar.Name = "CmbOrdenar";
+            this.CmbOrdenar.Size = new System.Drawing.Size(160, 33);
+            this.CmbOrdenar.TabIndex = 36;
+            // 
+            // BtnAscDes
+            // 
+            this.BtnAscDes.Location = new System.Drawing.Point(679, 90);
+            this.BtnAscDes.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnAscDes.Name = "BtnAscDes";
+            this.BtnAscDes.Size = new System.Drawing.Size(55, 33);
+            this.BtnAscDes.TabIndex = 38;
+            this.BtnAscDes.Text = "Asc";
+            this.BtnAscDes.UseVisualStyleBackColor = true;
+            this.BtnAscDes.Click += new System.EventHandler(this.BtnAscDes_Click);
             // 
             // FrmCatalogo
             // 
@@ -36,16 +148,39 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.ClientSize = new System.Drawing.Size(797, 376);
+            this.Controls.Add(this.BtnAscDes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CmbOrdenar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbFiltro);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.DtgDatos);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.TxtBuscar);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmCatalogo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmCatalogo";
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbFiltro;
+        private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.DataGridView DtgDatos;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.TextBox TxtBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CmbOrdenar;
+        private System.Windows.Forms.Button BtnAscDes;
     }
 }
--- SistemaBibliosfera/FrmCatalogo.cs
+++ SistemaBibliosfera/FrmCatalogo.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Manejadores;
+using Entidades;
 
 namespace SistemaBibliosfera
 {
     public partial class FrmCatalogo : Form
     {
+        ManejadorCatalogo Mc;
+        List<Autor> LibroAutores;
+        List<Categoria> LibroCategorias;
+        Libro libro;
+        int fila = 0, columna = 0;
         public FrmCatalogo()
         {
             InitializeComponent();
+            Mc = new ManejadorCatalogo();
+
+            LibroAutores = new List<Autor>();
+            LibroCategorias = new List<Categoria>();
+            libro = new Libro(0, "", "", 0, 0, LibroAutores, LibroCategorias);
+        }
+
+        private void DtgDatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAscDes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DtgDatos_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            fila = e.RowIndex; columna = e.ColumnIndex;
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
