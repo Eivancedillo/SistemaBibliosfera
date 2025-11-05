0d27811d3c60f0cbe6e16c4e66e05b73987d41f6
Eivan <eivan.ecedillo@gmail.com>
2025-11-04 20:30:42 -0600

    Inicio de modulo-Prestamos Entidades: - Creacion de entidad Manejadores: - ManejadorPrestamo empezado

--- Entidades/Prestamo.cs
+++ Entidades/Prestamo.cs
@@ -8,5 +8,12 @@ namespace Entidades
 {
     public class Prestamo
     {
+        public int IdPrestamo { get; set; }
+        public int NumeroControl { get; set; }
+        public int IdEjemplar { get; set; }
+        public string FechaPrestamo { get; set; }
+        public string FechaDevolucionPrevista { get; set; }
+        public string FechaDevolucionReal { get; set; }
+        public string EstadoPrestamo { get; set; }
     }
 }
--- Manejadores/ManejadorPrestamo.cs
+++ Manejadores/ManejadorPrestamo.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Entidades;
+using AccesoDatos;
 namespace Manejadores
 {
     public class ManejadorPrestamo
     {
+        Base b = new Base();
     }
 }
