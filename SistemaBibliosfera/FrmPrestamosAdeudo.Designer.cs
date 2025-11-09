d92cdac63c3a1b52a4b085229939f5f5d15e9b79
fridaquintero2020-oss <frida.quintero2020@institutorozcoyjimenez.edu.mx>
2025-11-08 18:48:33 -0600

    FrmPrestamosAdeudos terminado

--- SistemaBibliosfera/FrmPrestamosAdeudo.Designer.cs
+++ SistemaBibliosfera/FrmPrestamosAdeudo.Designer.cs
@@ -43,93 +43,96 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Location = new System.Drawing.Point(13, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.Size = new System.Drawing.Size(49, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Libro";
             // 
             // TxtLibro
             // 
-            this.TxtLibro.Location = new System.Drawing.Point(162, 23);
+            this.TxtLibro.Location = new System.Drawing.Point(180, 24);
             this.TxtLibro.Name = "TxtLibro";
-            this.TxtLibro.Size = new System.Drawing.Size(319, 30);
+            this.TxtLibro.Size = new System.Drawing.Size(354, 29);
             this.TxtLibro.TabIndex = 1;
             // 
             // TxtMonto
             // 
-            this.TxtMonto.Location = new System.Drawing.Point(162, 150);
+            this.TxtMonto.Location = new System.Drawing.Point(180, 158);
             this.TxtMonto.Name = "TxtMonto";
-            this.TxtMonto.Size = new System.Drawing.Size(98, 30);
+            this.TxtMonto.Size = new System.Drawing.Size(108, 29);
             this.TxtMonto.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 153);
+            this.label2.Location = new System.Drawing.Point(13, 161);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.Size = new System.Drawing.Size(122, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "Monto a pagar";
             // 
             // TxtFecha
             // 
-            this.TxtFecha.Location = new System.Drawing.Point(162, 109);
+            this.TxtFecha.Location = new System.Drawing.Point(180, 114);
             this.TxtFecha.Name = "TxtFecha";
-            this.TxtFecha.Size = new System.Drawing.Size(319, 30);
+            this.TxtFecha.Size = new System.Drawing.Size(354, 29);
             this.TxtFecha.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Location = new System.Drawing.Point(13, 118);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(140, 25);
+            this.label3.Size = new System.Drawing.Size(119, 21);
             this.label3.TabIndex = 4;
             this.label3.Text = "Fecha prevista";
             // 
             // BtnPagar
             // 
-            this.BtnPagar.Location = new System.Drawing.Point(137, 199);
+            this.BtnPagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(150)))), ((int)(((byte)(102)))));
+            this.BtnPagar.Location = new System.Drawing.Point(152, 209);
             this.BtnPagar.Name = "BtnPagar";
-            this.BtnPagar.Size = new System.Drawing.Size(108, 61);
+            this.BtnPagar.Size = new System.Drawing.Size(120, 64);
             this.BtnPagar.TabIndex = 6;
             this.BtnPagar.Text = "Pagar";
-            this.BtnPagar.UseVisualStyleBackColor = true;
+            this.BtnPagar.UseVisualStyleBackColor = false;
             this.BtnPagar.Click += new System.EventHandler(this.BtnPagar_Click);
             // 
             // BtnCancelar
             // 
-            this.BtnCancelar.Location = new System.Drawing.Point(251, 199);
+            this.BtnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(150)))), ((int)(((byte)(102)))));
+            this.BtnCancelar.Location = new System.Drawing.Point(279, 209);
             this.BtnCancelar.Name = "BtnCancelar";
-            this.BtnCancelar.Size = new System.Drawing.Size(108, 61);
+            this.BtnCancelar.Size = new System.Drawing.Size(120, 64);
             this.BtnCancelar.TabIndex = 7;
             this.BtnCancelar.Text = "Cancelar";
-            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.UseVisualStyleBackColor = false;
             this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
             // 
             // TxtCodigo
             // 
-            this.TxtCodigo.Location = new System.Drawing.Point(162, 68);
+            this.TxtCodigo.Location = new System.Drawing.Point(180, 71);
             this.TxtCodigo.Name = "TxtCodigo";
-            this.TxtCodigo.Size = new System.Drawing.Size(98, 30);
+            this.TxtCodigo.Size = new System.Drawing.Size(108, 29);
             this.TxtCodigo.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 71);
+            this.label4.Location = new System.Drawing.Point(13, 75);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 25);
+            this.label4.Size = new System.Drawing.Size(65, 21);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Codigo";
+            this.label4.Text = "Código";
             // 
             // FrmPrestamosAdeudo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(490, 272);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(544, 286);
             this.Controls.Add(this.TxtCodigo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.BtnCancelar);
@@ -140,7 +143,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.TxtLibro);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(38)))), ((int)(((byte)(31)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmPrestamosAdeudo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
