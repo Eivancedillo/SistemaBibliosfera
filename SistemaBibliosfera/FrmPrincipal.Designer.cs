153a5b8c94199db32e181427aa9bc41a05ab5eed
fridaquintero2020-oss <frida.quintero2020@institutorozcoyjimenez.edu.mx>
2025-11-10 21:56:14 -0600

    Cambios provisionales principal

--- SistemaBibliosfera/FrmPrincipal.Designer.cs
+++ SistemaBibliosfera/FrmPrincipal.Designer.cs
@@ -38,11 +38,24 @@
             this.BtnBibliotecarios = new System.Windows.Forms.ToolStripButton();
             this.BtnAsistencias = new System.Windows.Forms.ToolStripButton();
             this.BtnSalir = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -64,8 +77,9 @@
             // BtnDashboard
             // 
             this.BtnDashboard.AutoSize = false;
+            this.BtnDashboard.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnDashboard.BackgroundImage")));
+            this.BtnDashboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnDashboard.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("BtnDashboard.Image")));
             this.BtnDashboard.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnDashboard.Name = "BtnDashboard";
             this.BtnDashboard.Size = new System.Drawing.Size(64, 64);
@@ -75,8 +89,9 @@
             // BtnCatalogo
             // 
             this.BtnCatalogo.AutoSize = false;
+            this.BtnCatalogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnCatalogo.BackgroundImage")));
+            this.BtnCatalogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnCatalogo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnCatalogo.Image = ((System.Drawing.Image)(resources.GetObject("BtnCatalogo.Image")));
             this.BtnCatalogo.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnCatalogo.Name = "BtnCatalogo";
             this.BtnCatalogo.Size = new System.Drawing.Size(64, 64);
@@ -86,8 +101,9 @@
             // BtnPrestamos
             // 
             this.BtnPrestamos.AutoSize = false;
+            this.BtnPrestamos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnPrestamos.BackgroundImage")));
+            this.BtnPrestamos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnPrestamos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnPrestamos.Image = ((System.Drawing.Image)(resources.GetObject("BtnPrestamos.Image")));
             this.BtnPrestamos.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnPrestamos.Name = "BtnPrestamos";
             this.BtnPrestamos.Size = new System.Drawing.Size(64, 64);
@@ -97,8 +113,9 @@
             // BtnAdquisiciones
             // 
             this.BtnAdquisiciones.AutoSize = false;
+            this.BtnAdquisiciones.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnAdquisiciones.BackgroundImage")));
+            this.BtnAdquisiciones.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnAdquisiciones.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnAdquisiciones.Image = ((System.Drawing.Image)(resources.GetObject("BtnAdquisiciones.Image")));
             this.BtnAdquisiciones.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnAdquisiciones.Name = "BtnAdquisiciones";
             this.BtnAdquisiciones.Size = new System.Drawing.Size(64, 64);
@@ -108,8 +125,9 @@
             // BtnMiembros
             // 
             this.BtnMiembros.AutoSize = false;
+            this.BtnMiembros.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnMiembros.BackgroundImage")));
+            this.BtnMiembros.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnMiembros.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnMiembros.Image = ((System.Drawing.Image)(resources.GetObject("BtnMiembros.Image")));
             this.BtnMiembros.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnMiembros.Name = "BtnMiembros";
             this.BtnMiembros.Size = new System.Drawing.Size(64, 64);
@@ -119,8 +137,9 @@
             // BtnBibliotecarios
             // 
             this.BtnBibliotecarios.AutoSize = false;
+            this.BtnBibliotecarios.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnBibliotecarios.BackgroundImage")));
+            this.BtnBibliotecarios.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnBibliotecarios.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnBibliotecarios.Image = ((System.Drawing.Image)(resources.GetObject("BtnBibliotecarios.Image")));
             this.BtnBibliotecarios.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnBibliotecarios.Name = "BtnBibliotecarios";
             this.BtnBibliotecarios.Size = new System.Drawing.Size(64, 64);
@@ -130,8 +149,9 @@
             // BtnAsistencias
             // 
             this.BtnAsistencias.AutoSize = false;
+            this.BtnAsistencias.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnAsistencias.BackgroundImage")));
+            this.BtnAsistencias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnAsistencias.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnAsistencias.Image = ((System.Drawing.Image)(resources.GetObject("BtnAsistencias.Image")));
             this.BtnAsistencias.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnAsistencias.Name = "BtnAsistencias";
             this.BtnAsistencias.Size = new System.Drawing.Size(64, 64);
@@ -141,21 +161,153 @@
             // BtnSalir
             // 
             this.BtnSalir.AutoSize = false;
+            this.BtnSalir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnSalir.BackgroundImage")));
+            this.BtnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
             this.BtnSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnSalir.Name = "BtnSalir";
             this.BtnSalir.Size = new System.Drawing.Size(64, 64);
             this.BtnSalir.Text = "Salir";
             this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(916, 605);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(377, 434);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 65);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BiblioSfera";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.label2.Location = new System.Drawing.Point(409, 499);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Gestión bibliotecaria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label3.Location = new System.Drawing.Point(80, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dashboard";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label4.Location = new System.Drawing.Point(80, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cátalogo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label5.Location = new System.Drawing.Point(80, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Préstamos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label6.Location = new System.Drawing.Point(80, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Adquisiciones";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label7.Location = new System.Drawing.Point(80, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Miembros";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label8.Location = new System.Drawing.Point(80, 369);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Bibliotecarios";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label9.Location = new System.Drawing.Point(80, 434);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Asistencia";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.label10.Location = new System.Drawing.Point(80, 499);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 17);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Salir";
+            // 
             // FrmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.ClientSize = new System.Drawing.Size(976, 590);
             this.ControlBox = false;
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.toolStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -167,6 +319,7 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +336,16 @@
         private System.Windows.Forms.ToolStripButton BtnBibliotecarios;
         private System.Windows.Forms.ToolStripButton BtnAsistencias;
         private System.Windows.Forms.ToolStripButton BtnSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
