c7e65563a4f67e10f92f7e0df78884b6aac23478
Diana Veloz <dvelozpadilla@gmail.com>
2025-11-13 08:31:50 -0600

    Implementar diseño a los prestamos, asistencias, devoluciones.

--- SistemaBibliosfera/FrmDashboard.Designer.cs
+++ SistemaBibliosfera/FrmDashboard.Designer.cs
@@ -72,6 +72,12 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -91,6 +97,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // LblTotalLibros
@@ -334,61 +343,68 @@
             // 
             // groupBox6
             // 
+            this.groupBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox6.Controls.Add(this.label2);
             this.groupBox6.Controls.Add(this.LblPrestamosHoy);
-            this.groupBox6.Location = new System.Drawing.Point(759, 432);
+            this.groupBox6.Location = new System.Drawing.Point(6, 3);
             this.groupBox6.Name = "groupBox6";
             this.groupBox6.Size = new System.Drawing.Size(200, 83);
             this.groupBox6.TabIndex = 3;
             this.groupBox6.TabStop = false;
-            this.groupBox6.Text = "Prestamos hoy";
             // 
             // LblPrestamosHoy
             // 
             this.LblPrestamosHoy.AutoSize = true;
-            this.LblPrestamosHoy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblPrestamosHoy.Location = new System.Drawing.Point(72, 40);
+            this.LblPrestamosHoy.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPrestamosHoy.ForeColor = System.Drawing.Color.Peru;
+            this.LblPrestamosHoy.Location = new System.Drawing.Point(77, 31);
             this.LblPrestamosHoy.Name = "LblPrestamosHoy";
-            this.LblPrestamosHoy.Size = new System.Drawing.Size(37, 21);
+            this.LblPrestamosHoy.Size = new System.Drawing.Size(49, 30);
             this.LblPrestamosHoy.TabIndex = 0;
             this.LblPrestamosHoy.Text = "000";
             // 
             // groupBox7
             // 
+            this.groupBox7.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox7.Controls.Add(this.label3);
             this.groupBox7.Controls.Add(this.LblAsistenciasHoy);
-            this.groupBox7.Location = new System.Drawing.Point(759, 551);
+            this.groupBox7.Location = new System.Drawing.Point(6, 9);
             this.groupBox7.Name = "groupBox7";
             this.groupBox7.Size = new System.Drawing.Size(200, 83);
             this.groupBox7.TabIndex = 4;
             this.groupBox7.TabStop = false;
-            this.groupBox7.Text = "Asistencias hoy";
             // 
             // LblAsistenciasHoy
             // 
             this.LblAsistenciasHoy.AutoSize = true;
-            this.LblAsistenciasHoy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblAsistenciasHoy.Location = new System.Drawing.Point(72, 40);
+            this.LblAsistenciasHoy.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAsistenciasHoy.ForeColor = System.Drawing.Color.DarkOrange;
+            this.LblAsistenciasHoy.Location = new System.Drawing.Point(77, 37);
             this.LblAsistenciasHoy.Name = "LblAsistenciasHoy";
-            this.LblAsistenciasHoy.Size = new System.Drawing.Size(37, 21);
+            this.LblAsistenciasHoy.Size = new System.Drawing.Size(49, 30);
             this.LblAsistenciasHoy.TabIndex = 0;
             this.LblAsistenciasHoy.Text = "000";
             // 
             // groupBox9
             // 
+            this.groupBox9.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox9.Controls.Add(this.label4);
             this.groupBox9.Controls.Add(this.LblDevolucionesHoy);
-            this.groupBox9.Location = new System.Drawing.Point(759, 654);
+            this.groupBox9.Location = new System.Drawing.Point(5, 5);
             this.groupBox9.Name = "groupBox9";
             this.groupBox9.Size = new System.Drawing.Size(200, 83);
             this.groupBox9.TabIndex = 4;
             this.groupBox9.TabStop = false;
-            this.groupBox9.Text = "Devoluciones hoy";
             // 
             // LblDevolucionesHoy
             // 
             this.LblDevolucionesHoy.AutoSize = true;
-            this.LblDevolucionesHoy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblDevolucionesHoy.Location = new System.Drawing.Point(72, 40);
+            this.LblDevolucionesHoy.BackColor = System.Drawing.SystemColors.Control;
+            this.LblDevolucionesHoy.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDevolucionesHoy.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.LblDevolucionesHoy.Location = new System.Drawing.Point(77, 36);
             this.LblDevolucionesHoy.Name = "LblDevolucionesHoy";
-            this.LblDevolucionesHoy.Size = new System.Drawing.Size(37, 21);
+            this.LblDevolucionesHoy.Size = new System.Drawing.Size(49, 30);
             this.LblDevolucionesHoy.TabIndex = 0;
             this.LblDevolucionesHoy.Text = "000";
             // 
@@ -543,6 +559,67 @@
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Control;
+            this.panel4.Controls.Add(this.groupBox6);
+            this.panel4.Location = new System.Drawing.Point(739, 432);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(220, 93);
+            this.panel4.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(42, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 21);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Préstamos hoy";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Control;
+            this.panel5.Controls.Add(this.groupBox7);
+            this.panel5.Location = new System.Drawing.Point(739, 531);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(219, 99);
+            this.panel5.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(42, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 21);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Asistencias hoy";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.Control;
+            this.panel6.Controls.Add(this.groupBox9);
+            this.panel6.Location = new System.Drawing.Point(740, 639);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(218, 98);
+            this.panel6.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(33, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 21);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Devoluciones hoy";
+            // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -558,14 +635,14 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.groupBox9);
-            this.Controls.Add(this.groupBox7);
-            this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.LblBibliotecario);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmDashboard";
@@ -601,6 +678,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -652,5 +732,11 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel6;
     }
 }
