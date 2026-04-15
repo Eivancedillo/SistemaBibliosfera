06f8cfb6deda6ba1c1c1eb2976f3421771b899ba
Diana Veloz <dvelozpadilla@gmail.com>
2026-04-15 13:55:46 -0600

    Se implemento diseño completo al FrmPrestamos y se puede concluir que el diseño esta finalizado.

--- SistemaBibliosfera/FrmPrestamos.Designer.cs
+++ SistemaBibliosfera/FrmPrestamos.Designer.cs
@@ -37,41 +37,49 @@
             this.BtnCerrar = new System.Windows.Forms.Button();
             this.BtnAgregarr = new System.Windows.Forms.Button();
             this.BtnBuscarr = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(86)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(27, 5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 37);
+            this.label1.Size = new System.Drawing.Size(203, 50);
             this.label1.TabIndex = 31;
             this.label1.Text = "Préstamos";
             // 
             // CmbEstado
             // 
+            this.CmbEstado.BackColor = System.Drawing.Color.White;
             this.CmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CmbEstado.Font = new System.Drawing.Font("Segoe UI Light", 12F);
             this.CmbEstado.FormattingEnabled = true;
-            this.CmbEstado.Location = new System.Drawing.Point(797, 49);
+            this.CmbEstado.Location = new System.Drawing.Point(10, 5);
             this.CmbEstado.Name = "CmbEstado";
-            this.CmbEstado.Size = new System.Drawing.Size(134, 36);
+            this.CmbEstado.Size = new System.Drawing.Size(175, 36);
             this.CmbEstado.TabIndex = 30;
             // 
             // TxtBuscar
             // 
-            this.TxtBuscar.Location = new System.Drawing.Point(13, 49);
+            this.TxtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtBuscar.Location = new System.Drawing.Point(236, 61);
+            this.TxtBuscar.Multiline = true;
             this.TxtBuscar.Name = "TxtBuscar";
-            this.TxtBuscar.Size = new System.Drawing.Size(776, 34);
+            this.TxtBuscar.Size = new System.Drawing.Size(727, 34);
             this.TxtBuscar.TabIndex = 27;
             // 
             // DtgDatos
             // 
             this.DtgDatos.AllowUserToAddRows = false;
-            this.DtgDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(56)))), ((int)(((byte)(47)))));
+            this.DtgDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(86)))), ((int)(((byte)(102)))));
+            this.DtgDatos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.DtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
@@ -82,24 +90,26 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.DtgDatos.DefaultCellStyle = dataGridViewCellStyle1;
             this.DtgDatos.GridColor = System.Drawing.Color.DarkGray;
-            this.DtgDatos.Location = new System.Drawing.Point(12, 98);
+            this.DtgDatos.Location = new System.Drawing.Point(41, 110);
             this.DtgDatos.Name = "DtgDatos";
             this.DtgDatos.ReadOnly = true;
             this.DtgDatos.RowHeadersWidth = 51;
             this.DtgDatos.RowTemplate.Height = 24;
-            this.DtgDatos.Size = new System.Drawing.Size(1077, 285);
+            this.DtgDatos.Size = new System.Drawing.Size(1026, 285);
             this.DtgDatos.TabIndex = 26;
             this.DtgDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellClick);
             this.DtgDatos.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellEnter);
             // 
             // BtnCerrar
             // 
-            this.BtnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(150)))), ((int)(((byte)(102)))));
+            this.BtnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(153)))), ((int)(((byte)(88)))));
             this.BtnCerrar.FlatAppearance.BorderSize = 0;
             this.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnCerrar.Location = new System.Drawing.Point(976, 402);
+            this.BtnCerrar.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(244)))), ((int)(((byte)(213)))));
+            this.BtnCerrar.Location = new System.Drawing.Point(941, 404);
             this.BtnCerrar.Name = "BtnCerrar";
-            this.BtnCerrar.Size = new System.Drawing.Size(111, 47);
+            this.BtnCerrar.Size = new System.Drawing.Size(126, 42);
             this.BtnCerrar.TabIndex = 32;
             this.BtnCerrar.Text = "Aceptar";
             this.BtnCerrar.UseVisualStyleBackColor = false;
@@ -107,44 +117,73 @@
             // 
             // BtnAgregarr
             // 
+            this.BtnAgregarr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(153)))), ((int)(((byte)(88)))));
             this.BtnAgregarr.BackgroundImage = global::SistemaBibliosfera.Properties.Resources.icons8_añadir_60;
             this.BtnAgregarr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BtnAgregarr.FlatAppearance.BorderSize = 0;
             this.BtnAgregarr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnAgregarr.Location = new System.Drawing.Point(976, 47);
+            this.BtnAgregarr.Location = new System.Drawing.Point(1016, 57);
             this.BtnAgregarr.Name = "BtnAgregarr";
-            this.BtnAgregarr.Size = new System.Drawing.Size(40, 35);
+            this.BtnAgregarr.Size = new System.Drawing.Size(42, 43);
             this.BtnAgregarr.TabIndex = 78;
-            this.BtnAgregarr.UseVisualStyleBackColor = true;
+            this.BtnAgregarr.UseVisualStyleBackColor = false;
             this.BtnAgregarr.Click += new System.EventHandler(this.BtnAgregarr_Click);
             // 
             // BtnBuscarr
             // 
+            this.BtnBuscarr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(153)))), ((int)(((byte)(88)))));
             this.BtnBuscarr.BackgroundImage = global::SistemaBibliosfera.Properties.Resources.icons8_buscar_60;
             this.BtnBuscarr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.BtnBuscarr.FlatAppearance.BorderSize = 0;
             this.BtnBuscarr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnBuscarr.Location = new System.Drawing.Point(937, 47);
+            this.BtnBuscarr.Location = new System.Drawing.Point(969, 57);
             this.BtnBuscarr.Name = "BtnBuscarr";
-            this.BtnBuscarr.Size = new System.Drawing.Size(33, 36);
+            this.BtnBuscarr.Size = new System.Drawing.Size(42, 43);
             this.BtnBuscarr.TabIndex = 77;
-            this.BtnBuscarr.UseVisualStyleBackColor = true;
+            this.BtnBuscarr.UseVisualStyleBackColor = false;
             this.BtnBuscarr.Click += new System.EventHandler(this.BtnBuscarr_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(86)))), ((int)(((byte)(102)))));
+            this.label6.Location = new System.Drawing.Point(-5, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1106, 20);
+            this.label6.TabIndex = 91;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(244)))), ((int)(((byte)(213)))));
+            this.label11.Location = new System.Drawing.Point(-5, 27);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(1106, 10);
+            this.label11.TabIndex = 93;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(153)))), ((int)(((byte)(88)))));
+            this.panel1.Controls.Add(this.CmbEstado);
+            this.panel1.Location = new System.Drawing.Point(40, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1027, 46);
+            this.panel1.TabIndex = 94;
             // 
             // FrmPrestamos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(244)))), ((int)(((byte)(213)))));
             this.ClientSize = new System.Drawing.Size(1101, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.BtnAgregarr);
             this.Controls.Add(this.BtnBuscarr);
             this.Controls.Add(this.BtnCerrar);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.CmbEstado);
             this.Controls.Add(this.TxtBuscar);
             this.Controls.Add(this.DtgDatos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(38)))), ((int)(((byte)(31)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -154,6 +193,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmPestamos";
             ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,5 +207,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button BtnAgregarr;
         private System.Windows.Forms.Button BtnBuscarr;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel1;
     }
 }
